ffb95862652881df0eb4f9a41dc20df0d9da9ea6
Adrian Price-Whelan <adrian.prw@gmail.com>
2012-03-29 06:50:40 -0400

    load light curves update

--- correlateLightCurves.py
+++ correlateLightCurves.py
@@ -12,6 +12,7 @@ logging.basicConfig(level=logging.DEBUG)
 # Third party
 import numpy as np
 import pyfits as pf
+import matplotlib.pyplot as plt
 from sqlalchemy import func
 import apwlib.geometry as g
 import mlpy
@@ -21,33 +22,68 @@ from ptf.db.DatabaseConnection import *
 
 __author__ = "adrn@astro.columbia.edu"
 
-def correlation_function(number_of_points_range, number_of_light_curves=100):
-    """ Given the minimum and maximum number of observations,
-        compute the correlation function for how similar nearby
-        light curves are as a function of angular separation
+def correlation_function(light_curve):
+    """ Compute the 'correlation function' for a given light curve with 
+        its neighboring light curves
     """
+    
+    # Get all nearby light curves with +/- 10% of the number of points as the input
+    num_points = len(light_curve.Rmjd)
+    nearby_light_curves = session.query(LightCurve).filter(func.array_length(LightCurve.mjd, 1) > (num_points-0.1*num_points))\
+                                            .filter(func.array_length(LightCurve.mjd, 1) < (num_points+0.1*num_points))\
+                                            .filter(func.q3c_radial_query(LightCurve.ra, LightCurve.dec, light_curve.ra, light_curve.dec, 1.0))\
+                                            .all()
+    
+    mjd1 = light_curve.Rmjd
+    sorted_mjd1 = mjd1[np.argsort(mjd1)]
+    Rmag1 = light_curve.Rmag
+    sorted_Rmag1 = Rmag1[np.argsort(Rmag1)]
+    
     data = []
-    for ii in range(number_of_light_curves):
-        light_curve = session.query(LightCurve).filter(func.array_length(LightCurve.mjd, 1) > number_of_points_range[0])\
-                                                .filter(func.array_length(LightCurve.mjd, 1) < number_of_points_range[1])\
-                                                .order_by(func.random()).limit(1).one()
+    for lc in nearby_light_curves:
+        sorted_mjd2 = lc.Rmjd[np.argsort(lc.Rmjd)]
+        Rmag2 = lc.Rmag
+        sorted_Rmag2 = Rmag2[np.argsort(Rmag2)]
         
-        nearby_light_curves = session.query(LightCurve).filter(func.array_length(LightCurve.mjd, 1) > number_of_points_range[0])\
-                                                .filter(func.array_length(LightCurve.mjd, 1) < number_of_points_range[1])\
-                                                .filter(func.q3c_radial_query(LightCurve.ra, LightCurve.dec, light_curve.ra, light_curve.dec, 1.0))\
-                                                .all()
+        if len(sorted_mjd1) < len(sorted_mjd2):
+            sorted_filtered_Rmag2 = sorted_Rmag2[np.in1d(sorted_mjd2, sorted_mjd1)]
+            sorted_filtered_Rmag1 = sorted_Rmag1
+        elif len(sorted_mjd1) > len(sorted_mjd2):
+            sorted_filtered_Rmag1 = sorted_Rmag1[np.in1d(sorted_mjd1, sorted_mjd2)]
+            sorted_filtered_Rmag2 = sorted_Rmag2
+        else:
+            sorted_filtered_Rmag1 = sorted_Rmag1
+            sorted_filtered_Rmag2 = sorted_Rmag2
         
-        for lc in nearby_light_curves:
-            distance = g.subtends(light_curve.ra, light_curve.dec, lc.ra, lc.dec, units="degrees")
-            correlation = mlpy.dtw_std(light_curve.Rmag, lc.Rmag)
-            data.append((distance, correlation))
-        data = np.array(data, dtype=[("distance", float), ("correlation", float)]).view(np.recarray)
-        print max(data.distance), min(data.distance)
-        break
-        for bin in np.arange(0., 3600., 50.): #arcseconds
-            thisData = data[(data.distance > bin/3600.) & (data.distance < (bin+50)/3600.)]
-            #print np.sum(thisData.correlation) / float(len(thisData))
-            print np.median(thisData.correlation), np.mean(thisData.correlation)
+        distance = g.subtends(light_curve.ra, light_curve.dec, lc.ra, lc.dec, units="degrees")
+        correlation = mlpy.dtw_std(sorted_filtered_Rmag1, sorted_filtered_Rmag2)
+        data.append((distance, correlation))
+        
+        if correlation > 30000 or correlation < 3:
+            fig = plt.figure()
+            ax1 = fig.add_subplot(211)
+            ax1.plot(sorted_mjd1, sorted_Rmag1, 'k.')
+            
+            ax2 = fig.add_subplot(212)
+            ax2.plot(sorted_mjd2, sorted_Rmag2, 'k.')
+            
+            fig.savefig("{0}_{1}.png".format(light_curve.objid, lc.objid))
+
+    data = np.array(data, dtype=[("distance", float), ("correlation", float)]).view(np.recarray)
+    print max(data.distance), min(data.distance)
+    
+    fig2 = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.semilogy(data.distance, data.correlation, 'ko')
+    fig.savefig("plots/correlation.pdf")
+    
+    return
+    
+    for bin in np.arange(0., 3600., 50.): #arcseconds
+        thisData = data[(data.distance > bin/3600.) & (data.distance < (bin+50)/3600.)]
+        #print np.sum(thisData.correlation) / float(len(thisData))
+        print np.median(thisData.correlation), np.mean(thisData.correlation)
             
 if __name__ == "__main__":
-    correlation_function((100,150), 1)
+    lc = session.query(LightCurve).filter(func.array_length(LightCurve.mjd, 1) > 1000).limit(1).one()
+    correlation_function(lc)
--- loadLightCurves.py
+++ loadLightCurves.py
@@ -10,8 +10,47 @@
 import os, sys
 from argparse import ArgumentParser
 import logging
+import pyfits as pf
+import numpy as np
 
-from db.util import loadLightCurves
+from ptf.db.DatabaseConnection import *
+from ptf.db.NumpyAdaptors import *
+
+def loadLightCurves(filename):
+    """ """
+    
+    logging.debug("Opening {}...".format(filename))
+    f = pf.open(filename)
+    resultsArray = f[1].data.view(np.recarray)
+    f.close()
+    logging.debug("File loaded!")
+    
+    session.begin()
+    logging.debug("Starting database load...")
+    for objid in np.unique(resultsArray.objid):
+        lightCurveData = resultsArray[resultsArray.objid == objid]
+        if len(lightCurveData.mag) < 10: continue
+        
+        lightCurve = LightCurve()
+        lightCurve.objid = np.array([objid]).astype(np.uint64)[0]
+        lightCurve.mag = lightCurveData.mag
+        lightCurve.mag_error = lightCurveData.mag_err
+        lightCurve.mjd = lightCurveData.mjd
+        lightCurve.sys_error = lightCurveData.sys_err
+        lightCurve.ra = lightCurveData.ra[0]
+        lightCurve.dec = lightCurveData.dec[0]
+        lightCurve.flags = lightCurveData["flags"].astype(np.uint16)
+        lightCurve.imaflags = lightCurveData.imaflags_iso.astype(np.uint16)
+        lightCurve.filter_id = lightCurveData.filter_id.astype(np.uint8)
+        session.add(lightCurve)
+        
+        if len(session.new) == 1000:
+            session.commit()
+            logging.debug("1000 light curves committed!")
+            session.begin()
+    
+    logging.info("All light curves committed!")
+    session.commit()
 
 if __name__ == "__main__":    
     parser = ArgumentParser()
@@ -25,8 +64,8 @@ if __name__ == "__main__":
         verbosity = logging.DEBUG
     else:
         verbosity = logging.INFO
-        
-    loadLightCurves(args.filename, verbosity=verbosity)
+    
+    loadLightCurves(args.filename)
     
 else:
     raise ImportError("loadLightCurves.py should be run as a standalone script, not imported!")
