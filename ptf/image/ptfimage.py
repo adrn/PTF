2b1f2438270bf93db416a19d4e68132ee7411e46
adrn <adrian.prw@gmail.com>
2012-07-16 15:12:14 -0400

    added new module for downloading PTF images

--- ptf/image/ptfimage.py
+++ ptf/image/ptfimage.py
@@ -9,18 +9,17 @@ import abc
 import base64
 import os
 import pytest
+import re
 import urllib, urllib2
 import cStringIO as StringIO
 
 # Third-party
+import pyfits as pf
 try:
     from apwlib.globals import greenText, yellowText, redText
     import apwlib.geometry as g
 except ImportError:
     raise ImportError("apwlib not found! \nDo: 'git clone git@github.com:adrn/apwlib.git' and run 'python setup.py install' to install.")
-
-# Project
-
 
 # These URLs might change in the future, but they provide HTTP-level access to
 #   the PTF imaging data API at IPAC.
@@ -31,9 +30,11 @@ try:
     IPAC_PASSWORD = os.environ["IPAC_PASSWORD"]
 except KeyError:
     raise KeyError("Environment has no IPAC_USER or IPAC_PASSWORD. You must set these environment variables before running this script!")
+SEARCH_COLUMNS = ['expid', 'obsdate', 'crval1', 'crval2', 'filter', 'ccdid', 'ptffield', 'seeing', 'airmass', 'moonillf', 'moonesb', 'photcalflag', 'infobits', 'nid', 'fieldid', 'ptfpid', 'pfilename', 'afilename1', 'obsmjd', 'ptfprpi', 'filtersl', 'moonra', 'moondec', 'moonphas', 'moonalt', 'ra1', 'dec1', 'ra2', 'dec2', 'ra3', 'dec3', 'ra4', 'dec4', 'gain', 'readnoi', 'darkcur']
 
-def send_ipac_search_request(url):
-    """ Given a full URL, open an HTTP request to the url and expect a table to be returned 
+def retrieve_ipac_file(url):
+    """ Given a full URL, open an HTTP request to the url and return a StringIO file object
+        with the contents of the response.
         
         Parameters
         ----------
@@ -41,6 +42,7 @@ def send_ipac_search_request(url):
             A full IPAC url, e.g. http://kanaloa.ipac...etc.../process?query=blah
 
     """
+    
     request = urllib2.Request(url)
     
     # Encode the username and password to send for authorization
@@ -60,67 +62,129 @@ def send_ipac_search_request(url):
     file = StringIO.StringIO(response.read())
     return file
 
-def test_send_ipac_search_request():
-    # An example url
-    url = "http://kanaloa.ipac.caltech.edu/ibe/search/ptf/dev/process?POS=215.1351,31.1241"
-    table_file = send_ipac_search_request(url)
+def parse_ipac_table(table_file):
+    """ Given a text file output from an IPAC query, parse it and return a list
+        of metadata objects (currently dictionaries)
+    """
+    file_lines = table_file.readlines()
+    if len(file_lines) < 5:
+        raise ValueError("No images found!")
+    
+    columns = file_lines[0].replace("|","   ").split()
+    
+    # Each row in the table starting at index 4 is metadata for a new image / observation
+    metadatas = []
+    for image_data in file_lines[4:]:
+        line = image_data.replace("|","   ").split()
+        obsdate_idx = columns.index("obsdate")
+        tmp = line[obsdate_idx] + " " + line[obsdate_idx+1]
+        del line[obsdate_idx+1]
+        line[obsdate_idx] = tmp
+        metadatas.append(dict(zip(columns, line)))
+    
+    return metadatas
+
+def download_images_from_metadatas(metadatas):
+    """ Given a list of metadata instances, download the 'pfilename' files
+        and return PTFImage objects for each file
+    """
+    ptf_images = []
+    for metadata in metadatas:
+        url = os.path.join(IPAC_DATA_URL, metadata["pfilename"])
+        ptf_images.append(PTFImage(retrieve_ipac_file(url), metadata=metadata))
+        print greenText("Image {} downloaded.")
+    
+    return ptf_images
 
 class PTFImage(object):
     
-    def __init__(self, fits_file, mask_file=None):
-        pass
+    def __init__(self, fits_file, mask_file=None, metadata=None):
+        self.fits = pf.open(fits_file)
+        self.metadata = metadata
     
-    def from_metadata(metadata):
-        """ Initialize a PTFImage object from image metadata """
-        fits_filename = metadata["pfilename"]
-        mask_filename = metadata["afilename1"]
+    def save(self, path, filename=None):
+        """ Given a path, save the PTFImage as a FITS file in that path """
         
-        # Retrieve FITS files
-        print IPAC_DATA_URL, fits_filename
+        if filename is None and self.metadata is None:
+            raise ValueError("If the image has no 'metadata', you must specify a filename")
+        elif filename is not None:
+            pass
+        elif filename is None and self.metadata is not None:
+            filename = os.path.basename(self.metadata["pfilename"])
         
-        img = PTFImage(fits_file, mask_file)
-        img.metadata = metadata
-        return img
+        self.fits.writeto(os.path.join(path, filename))
 
-def test_ptfimage():
-    # Test that we can't create an instance of the abstract class PTFImage
-    with pytest.raises(TypeError):
-        image = PTFImage()
+def ptf_images_from_fieldid(fieldid, ccds=[], filter="R", epoch=None):
+    """ Creates PTF FITS Images given a PTF Field ID and optionally CCD IDs.
+        
+        Parameters
+        ----------
+        fieldid : int
+            A PTF Field ID
+        ccds : (optional) list
+            A list of CCD IDs
+        filter : (optional) str
+            Select only observations of this filter.
+        epoch : (optional) float
+            The MJD of the observation of the image. If not specified, the
+            image with the best seeing is returned.
+    """
+    
+    # Construct search URL with parameters
+    where_str = "?where=filter IN ('{}')".format(filter)
+    
+    where_str += " AND ptffield IN ({})".format(fieldid)
+    
+    if len(ccds) > 0:
+        where_str += " AND ccdid IN ({})".format(",".join([str(x) for x in ccds]))
+    
+    if epoch is not None:
+        where_str += " AND obsmjd IN ({})".format(epoch)
+    
+    table_file = retrieve_ipac_file(IPAC_SEARCH_URL + urllib.quote(where_str) + "&columns={}".format(",".join(SEARCH_COLUMNS)))
+    metadatas = parse_ipac_table(table_file)
+    ptf_images = download_images_from_metadatas(metadatas)
+    
+    return ptf_images
 
-class PTFFITSImage(PTFImage):
+def ptf_images_from_name(name, size, intersect="overlaps", filter="R", epoch=None):
+    """ Creates PTF FITS Images given a name of an astronomical object that can be
+        resolved by SIMBAD.
+        
+        Parameters
+        ----------
+        name : str
+            The name of an astronomical object.
+        size : apwlib.geometry.Angle, tuple
+            An angular extent on the sky, or a tuple of 2 angular extents
+            representing a size in RA and a size in Dec.
+        intersect : str
+            See IPAC image query documentation:
+                http://kanaloa.ipac.caltech.edu/ibe/queries.html
+        filter : (optional) str
+            Select only observations of this filter.
+        epoch : (optional) float
+            The MJD of the observation of the image. If not specified, the
+            image with the best seeing is returned.
+    """
     
-    def __init__(self):
-        raise NotImplementedError()
+    url = "http://simbad.u-strasbg.fr/simbad/sim-id?Ident={}&NbIdent=1&Radius=2&Radius.unit=arcmin&submit=submit+id&output.format=ASCII_TAB".format(urllib.quote(name))
+    request = urllib2.Request(url)
+    response = urllib2.urlopen(request)
+    
+    pattr = re.compile("Coordinates\(ICRS\,ep\=J2000\,eq\=2000\)\: ([0-9\s\+\-\.]+)\(")
+    try:
+        radec_str = pattr.search(response.read()).groups()[0]
+    except AttributeError:
+        raise ValueError(redText("Unable to resolve name '{}'".format(name)))
+    
+    ra,dec = radec_str.strip().split("  ")
+    ra = g.RA(ra)
+    dec = g.Dec(dec)
+    
+    return ptf_images_from_position(ra, dec, size=size, intersect=intersect, filter=filter, epoch=epoch)
 
-class TestPTFFITSImage:
-    ra = g.RA("14:21:17.1341")
-    dec = g.Dec("+15:16:51.6246")
-    x_size = g.Angle.fromDegrees(0.2)
-    y_size = g.Angle.fromDegrees(0.1)
-    
-    def test_failures(self):
-        with pytest.raises(TypeError):
-            # Test failure when passing in float 'size'
-            ptf_image = PTFFITSImage.from_position(ra=self.ra, dec=self.dec, size=0.1512)
-            
-    def test_api(self):
-        # Test extracting square image
-        ptf_image = PTFFITSImage.from_position(ra=self.ra, dec=self.dec, size=self.x_size)
-        
-        # Test creating rectangular image
-        ptf_image = PTFFITSImage.from_position(ra=self.ra, dec=self.dec, size=(self.x_size,self.y_size))
-        
-        
-""" def from_position(ra, dec, size, filter="R", epoch=None):
-        return
-    
-    def from_name(name, size, filter="R", epoch=None):
-        return
-    
-    def from_fieldid(fieldid, ccds, filter="R", epoch=None):
-        return
-"""
-def ptf_images_from_position(ra, dec, size, filter="R", epoch=None):
+def ptf_images_from_position(ra, dec, size, intersect="covers", filter="R", epoch=None):
     """ Creates PTF FITS Images given an equatorial position (RA/Dec) 
         and a size.
         
@@ -133,6 +197,9 @@ def ptf_images_from_position(ra, dec, size, filter="R", epoch=None):
         size : apwlib.geometry.Angle, tuple
             An angular extent on the sky, or a tuple of 2 angular extents
             representing a size in RA and a size in Dec.
+        intersect : str
+            See IPAC image query documentation:
+                http://kanaloa.ipac.caltech.edu/ibe/queries.html
         filter : (optional) str
             Select only observations of this filter.
         epoch : (optional) float
@@ -152,39 +219,73 @@ def ptf_images_from_position(ra, dec, size, filter="R", epoch=None):
     
     # Construct search URL with parameters
     pos_str = "POS={ra.degrees},{dec.degrees}".format(ra=ra, dec=dec)
-    search_url_append =  "?{}&{}&where=filter IN ('{}')".format(pos_str, size_str, filter)
+    intersect_str = "INTERSECT={}".format(intersect.upper())
+    
+    search_url_append =  "?{}&{}&{}&where=filter IN ('{}')".format(pos_str, size_str, intersect_str, filter)
     
     if epoch is not None:
         search_url_append += " AND obsmjd IN ({})".format(epoch)
     
-    table_file = send_ipac_search_request(IPAC_SEARCH_URL + urllib.quote(search_url_append))
+    table_file = retrieve_ipac_file(IPAC_SEARCH_URL + urllib.quote(search_url_append) + "&columns={}".format(",".join(SEARCH_COLUMNS)))
     
-    file_lines = table_file.readlines()
-    if len(file_lines) < 5:
-        raise ValueError("No images found!")
+    metadatas = parse_ipac_table(table_file)
     
-    columns = file_lines[0].replace("|","   ").split()
+    ptf_images = []
+    for metadata in metadatas:
+        cutout_url = os.path.join(IPAC_DATA_URL, metadata["pfilename"])
+        cutout_url_query = "?center={ra.degrees},{dec.degrees}&{size}&gzip=false".format(ra=ra, dec=dec, size=size_str.lower())
+        
+        ptf_images.append(PTFImage(retrieve_ipac_file(cutout_url + cutout_url_query), metadata=metadata))
+        print greenText("Image {} downloaded.")
     
-    # Each row in the table starting at index 4 is metadata for a new image / observation
-    metadatas = []
-    for image_data in file_lines[4:]:
-        line = image_data.replace("|","   ").split()
-        tmp = line[4] + " " + line[5]
-        del line[5]
-        line[4] = tmp
-        metadatas.append(dict(zip(columns, line)))
+    return ptf_images
+
+# ===============================================
+# ==================== Tests ====================
+# ===============================================
+
+def test_retrieve_ipac_file():
+    # An example url
+    url = "http://kanaloa.ipac.caltech.edu/ibe/search/ptf/dev/process?POS=215.1351,31.1241"
+    table_file = retrieve_ipac_file(url + "&columns={}".format(",".join(SEARCH_COLUMNS)))
     
-    return [PTFFITSImage.from_metadata(metadata) for metadata in metadatas]
+    url = "http://kanaloa.ipac.caltech.edu/ibe/data/ptf/dev/process/proc/2010/02/15/f2/c0/p13/v1/PTF_201002153960_i_p_scie_t093015_u011664659_f02_p110001_c00.fits"
+    image_file = retrieve_ipac_file(url)
+
+def test_ptfimage():
+    # Get an image:
+    image = ptf_images_from_fieldid(110001, ccds=[0], epoch=55242.39601)[0]
+    
+    # Test save()
+    image.save("/tmp/")
+    image.save("/tmp/", "ptf_test_image.fits")
+    
+    assert os.path.exists("/tmp/{}".format(image.metadata["pfilename"]))
+    assert os.path.exists("/tmp/ptf_test_image.fits")
+
+def test_ptf_images_from_fieldid():
+    fieldid = 110001
+    ptf_images_from_fieldid(fieldid, epoch=55242.39601)
+
+def test_ptf_images_from_name():
+    names = ["m42", "ngc752", "m101", "m31", "orion", "albireo"]
+    size = g.Angle.fromDegrees(0.05)
+    
+    for name in names:
+        # Test a small patch, contained on one CCD
+        ptf_images_from_name(name=name, size=size)
     
 def test_ptf_images_from_position():
     ra = g.RA("14:21:17.1341")
     dec = g.Dec("+15:16:51.6246")
-    x_size = g.Angle.fromDegrees(0.2)
-    y_size = g.Angle.fromDegrees(0.1)
+    x_size = g.Angle.fromDegrees(0.05)
+    y_size = g.Angle.fromDegrees(1.0)
     
+    # Test a small patch, contained on one CCD
     ptf_images_from_position(ra=ra, dec=dec, size=x_size)
+    
+    # Test a large patch, image is over multiple CCDs and size is a tuple
+    ptf_images_from_position(ra=ra, dec=dec, size=(x_size,y_size), intersect="overlaps")
     
     with pytest.raises(ValueError):
         ptf_images_from_position(ra=ra, dec=dec, size=x_size, filter="g")
-    
-    return PTFFITSImage()
