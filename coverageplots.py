9015f5112537dfbf3c3049f40e107d0d020f1fe0
adrn <adrian.prw@gmail.com>
2012-07-19 17:46:59 -0400

    added new code to make json file for sexy visualization

--- coverageplots.py
+++ coverageplots.py
@@ -9,13 +9,19 @@
 
 # Standard library
 import sys, os
+import copy
 import cPickle as pickle
+import json
+import math
 
 # Third-party
 import apwlib.convert as c
 import apwlib.geometry as g
+
 import matplotlib
-matplotlib.use("Agg")
+import matplotlib.cm as cm
+import matplotlib.colors as mc
+
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
 import numpy as np
@@ -23,11 +29,139 @@ import pyfits as pf
 from scipy.stats import scoreatpercentile
 
 # PTF
-from ptf.globals import *
+from ptf.globals import camera_size_degrees
+
+def observations_per_field(field_data, filename="data/field_observations.npy"):
+    """ Determine the number of observations per field, given an array of 
+        field data. Returns a dictionary, keyed by PTF Field IDs
+    """
+    
+    if not os.path.exists(filename):
+        unq_field_ids = np.unique(field_data.field_id)
+        
+        field_observations = []
+        for field_id in unq_field_ids:
+            this_field = field_data[field_data["field_id"] == field_id]
+            
+            obs_nums = []
+            for ccd_id in np.unique(this_field["ccd_id"]):
+                obs_nums.append(sum(this_field["ccd_id"] == ccd_id))
+            
+            field_observations.append((field_id,max(obs_nums)))
+        
+        arr = np.array(field_observations, dtype=[("field_id", int), ("observations", int)])    
+        np.save(filename, arr)
+    else:
+        arr = np.load(filename)
+    
+    return arr
+
+def field_exposure_data_to_feature(field_data):
+    """ Converts an array of exposure data for a single field into a
+        'feature' object to be stuffed into JSON.
+    """
+    feature = dict(type="Feature", id=str(field_data["id"][0]))
+    properties = dict(name=str(field_data["id"][0]))
+    geometry = dict(type="Polygon")
+    
+    ra = field_data["ra"][0]-180.0
+    dec = field_data["dec"][0]
+    
+    ra_offset = camera_size_degrees[0]/math.cos(math.radians(dec))/2.
+    dec_offset = camera_size_degrees[1]/2.
+    
+    min_ra = ra - ra_offset
+    max_ra = ra + ra_offset
+    
+    min_dec = dec - dec_offset
+    max_dec = dec + dec_offset
+    
+    coordinates = [[ [min_ra, min_dec], [max_ra, min_dec], [max_ra, max_dec], [min_ra, max_dec], [min_ra, min_dec] ]]
+    geometry["coordinates"] = coordinates
+    feature["geometry"] = geometry
+    feature["properties"] = properties
+
+    return feature
+
+def fits_to_json(exposure_data_file):
+    """ Converts a FITS file containing exposure information into a JSON file
+        with a list of Features to be displayed by D3's GeoJson plugin.
+        
+        The final structure should look like this, where "name" is the field id,
+        and coordinates are a list of the coordinates of the 4 corners of the field.
+        
+            {"type" : "FeatureCollection",
+                "features": [
+                    {"type" : "Feature",
+                     "properties" : { "name" : "2471"},
+                                      "geometry": { "type" : "Polygon",
+                                                    "coordinates" : [[ [100.0, 0.0], [101.0, 0.0], [101.0, 1.0], [100.0, 1.0], [100.0, 0.0] ]]
+                                                  },
+                     "id":"2471"}, etc.
+                            ]
+            }        
+    """
+    
+    final_dict = dict(type="FeatureCollection", features=[])
+    
+    fields = np.load("data/all_fields.npy")
+    raw_field_data = pf.open(exposure_data_file)[1].data
+    obs_num_per_field = observations_per_field(raw_field_data)
+    
+    # Minimum and maximum number of observations for all fields
+    min_obs = 1
+    # There is one crazy outlier: the Orion field, so I remove that for calculating the max...
+    obs_copy = copy.copy(obs_num_per_field["observations"])
+    obs_copy[obs_num_per_field["observations"].argmax()] = 0.0
+    max_obs = obs_copy.max()
+    
+    # Create a matplotlib lognorm scaler between these values
+    scaler = matplotlib.colors.LogNorm(vmin=min_obs, vmax=max_obs)
+    
+    ptf_fields = []
+    for row in obs_num_per_field:
+        field_id = row["field_id"]
+        this_field = fields[fields["id"] == field_id]
+        if len(this_field) == 0 or field_id <= 642:
+            print "ERROR", field_id
+            continue
+        
+        if this_field["dec"][0] < -40:
+            print "WTF", field_id
+        
+        feature = field_exposure_data_to_feature(this_field)
+        
+        # Determine color of field
+        rgb = cm.autumn(scaler(row["observations"]))
+        feature["properties"]["color"] = mc.rgb2hex(rgb)
+        feature["properties"]["alpha"] = scaler(row["observations"])*0.5 + 0.2
+        feature["properties"]["number_of_observations"] = str(row["observations"])
+        feature["properties"]["ra"] = str(this_field["ra"][0])
+        feature["properties"]["dec"] = str(this_field["dec"][0])
+        
+        final_dict["features"].append(feature)
+    
+    return json.dumps(final_dict)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # =~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~
+# OLD CODE!
+# =~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~
 
-"""
 class CCD(object):
     
     def __init__(self, table_data):
@@ -44,7 +178,6 @@ class CCD(object):
     @property
     def baseline(self):
         return max(self.mjd) - min(self.mjd)
-"""
 
 class Field(object):
     """ Abstract Field class """
@@ -226,7 +359,7 @@ def new_2d_plot():
     
     return 
 
-if __name__ == "__main__":
+if False:
     # PTF:
     raw_field_data = pf.open("data/exposureData.fits")[1].data
     unq_field_ids = np.unique(raw_field_data.field_id)
@@ -234,11 +367,12 @@ if __name__ == "__main__":
     ptf_fields = []
     for field_id in unq_field_ids:
         one_field_data = raw_field_data[raw_field_data.field_id == field_id]
-        mean_ra = np.mean(one_field_data.ra) / 15.
-        mean_dec = np.mean(one_field_data.dec)
-        observations = len(one_field_data) / len(np.unique(one_field_data.ccd_id))
+        #mean_ra = np.mean(one_field_data.ra) / 15.
+        #mean_dec = np.mean(one_field_data.dec)
+        #observations = len(one_field_data) / len(np.unique(one_field_data.ccd_id))
         
-        ptf_fields.append(PTFField(mean_ra, mean_dec, id=field_id, number_of_observations=observations))
+        #ptf_fields.append(PTFField(mean_ra, mean_dec, id=field_id, number_of_observations=observations))
+        ptf_fields.append(PTFField(one_field_data))
     
     # OGLE:
     high_cadence = np.genfromtxt("data/ogle4_common.txt", names=["ra","dec","l","b"], usecols=[6,7,8,9]).view(np.recarray)
