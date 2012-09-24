6742b1876348dbf9e0fbc042839198505c19bcd7
adrn <adrian.prw@gmail.com>
2012-09-23 17:20:11 -0700

    globals

--- ptf/globals.py
+++ ptf/globals.py
@@ -6,6 +6,7 @@ camera_size = (12000., 8000.) #pixels
 ccd_size = (2048, 4096) # x, y
 
 camera_size_degrees = (camera_size[0]*pix_scale/3600., camera_size[1]*pix_scale/3600.)
+camera_size_radius = ((camera_size_degrees[0]/2.)**2 + (camera_size_degrees[1]/2.)**2)**0.5
 
 # OGLE IV field sizes
 ogle_camera_size = (1.225,0.9) #degrees
@@ -25,4 +26,16 @@ with open(os.path.join(_base_path, "config"), "r") as f:
             config[key] = int(val)
         except ValueError:
             config[key] = val
-        
+
+# Convert my names for variability indices to the PDB names
+pdb_index_name = dict(eta="vonNeumannRatio", j="stetsonJ", k="stetsonK", delta_chi_squared="chiSQ", sigma_mu=["magRMS","referenceMag"])
+
+def source_index_name_to_pdb_index(source, index_name):
+    """ Given a source (a row from chip.sources) and an index name (e.g. eta),
+        return the value of the statistic. This is particularly needed for a
+        computed index like sigma_mu.
+    """
+    if index_name == "sigma_mu":
+        return source[pdb_index_name[index_name][0]] / source[pdb_index_name[index_name][1]]            
+    else:
+        return source[pdb_index_name[index_name]]
