d23501a09f10dcc305a6227f43e1f911d856b36e
adrn <adrian.prw@gmail.com>
2012-10-21 23:56:39 -0400

    move all light curve classes to same module

--- ptf/lightcurve/lightcurve.py
+++ ptf/lightcurve/lightcurve.py
@@ -11,10 +11,9 @@ import logging
 import numpy as np
 import matplotlib.pyplot as plt
 
-# PTF
-import galacticutils
+from ..analyze import microlensing_model
 
-# ~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=
+__all__ = ["PTFLightCurve", "PDBLightCurve", "SimulatedLightCurve"]
 
 class PTFLightCurve(object):
     """ Represents a PTF Light Curve """
@@ -54,7 +53,7 @@ class PTFLightCurve(object):
         self.mag = self.mag[idx]
         
         np.random.shuffle(idx)
-        self.error = self.mag[idx]
+        self.error = self.error[idx]
     
     def plot(self, ax=None, **kwargs):
         """ Either plots the light curve and show()'s it to the display, or plots it on 
@@ -122,6 +121,7 @@ class PTFLightCurve(object):
     
     def sdss_colors(self, mag_type="psf"):
         """ Returns a dictionary with SDSS colors for this object. mag_type can be 'psf' or 'mod' """
+        import galacticutils
         try:
             sdssData = galacticutils.querySDSSCatalog(self.ra, self.dec)
         except RuntimeError:
@@ -152,3 +152,103 @@ class PDBLightCurve(PTFLightCurve):
         self.source_id = int(source_id)
         
         super(PDBLightCurve, self).__init__(mjd, mag, error, **kwargs)
+
+class SimulatedLightCurve(PTFLightCurve):
+    
+    @staticmethod
+    def from_ptflightcurve(ptflightcurve):
+        return SimulatedLightCurve(mjd=ptflightcurve.mjd, mag=ptflightcurve.mag, error=ptflightcurve.error)
+    
+    def __init__(self, mjd, mag=None, error=None, outliers=False):
+        """ Creates a simulated PTF light curve
+        
+            Parameters
+            ----------
+            mjd : numpy.array
+                An array of mjd values. If none, creates one internally.
+            error : numpy.array
+                An array of error values (sigmas). If none, creates one internally.
+            mag : numpy.array
+                An optional array of magnitude values
+            outliers : bool, optional
+                This controls whether to sample from an outlier distribution
+                when creating magnitude values for the light curve
+            
+        """
+        
+        self.amjd = self.mjd = np.array(mjd)
+        
+        if error != None:
+            self.error = np.array(error)
+            if len(self.error) == 1:
+                self.error = np.zeros_like(self.mjd) + error
+            elif len(self.error) != len(self.mjd):
+                raise ValueError("Error array should have same shape as mjd")
+        else:
+            self.error = np.zeros_like(self.mjd)
+        
+        if isinstance(mag, np.ndarray) or isinstance(mag, list):
+            self.amag = self.mag = np.array(mag)
+        elif isinstance(mag, int) or isinstance(mag, float):
+            self.amag = self.mag = np.zeros(len(mjd)) + mag
+            self._addNoise()
+        else:
+            if outliers:
+                # Add ~1% outliers
+                outlier_points = (np.random.uniform(0.0, 1.0, size=len(self.mjd)) < 0.01).astype(float) * np.random.normal(0.0, 2.0, size=len(self.mjd))
+            else:
+                outlier_points = np.zeros(len(self.mjd))
+            
+            self.F0 = np.random.uniform(0.1, 1.) / 100.
+            self.mag = np.ones(len(self.mjd), dtype=float)*FluxToRMag(self.F0) + outlier_points
+    
+            self._addNoise()
+        
+        self._original_mag = copy.copy(self.mag)
+    
+    def reset(self):
+        self.amag = self.mag = copy.copy(self._original_mag)
+        self.u0 = None
+        self.t0 = None
+        self.tE = None
+    
+    def addMicrolensingEvent(self, u0=None, t0=None, tE=None, **kwargs):
+        """ Adds a simulated microlensing event to the light curve
+            
+            u0 : float, optional
+                The impact parameter for the microlensing event. If not specified,
+                the value will be drawn from the measured u0 distribution 
+            t0 : float, optional
+                The peak time of the event (shouldn't really be specified)
+                This is just drawn from a uniform distribution between mjd_min
+                and mjd_max
+            tE : float, optional
+                The length of the microlensing event. If not specified,
+                the value will be drawn from the measured tE distribution       
+        """
+        
+        # If u0 is not specified, draw from u0 distribution
+        #   - see for example Popowski & Alcock 
+        if u0 == None: self.u0 = np.random.uniform(0., 1.34)
+        else: self.u0 = float(u0)
+        
+        # If t0 is not specified, draw from uniform distribution between days
+        if t0 == None: self.t0 = np.random.uniform(min(self.mjd), max(self.mjd))
+        else: self.t0 = float(t0)
+        
+        if (self.t0 > max(self.mjd)) or (self.t0 < min(self.mjd)):
+            logging.warn("t0 is outside of the mjd range for this light curve!")
+        
+        # If tE is not specified, draw from tE distribution
+        #   I use an estimate of Wood's "observed" distribution for now:
+        #   http://onlinelibrary.wiley.com/store/10.1111/j.1365-2966.2005.09357.x/asset/j.1365-2966.2005.09357.x.pdf?v=1&t=h1whtf1h&s=7b4d93a69aa684387a49ece5fc33c32fa5037052
+        if tE == None: self.tE = 10**np.random.normal(1.3, 0.5)
+        else: self.tE = float(tE)
+        
+        params = {"t0" : self.t0, "tE" : self.tE, "u0" : self.u0, "m0" : 1.}
+        self.amag = self.mag = self.mag+microlensing_model(params, self.mjd)
+    
+    def _addNoise(self):
+        """ Add scatter to the light curve """
+        if not np.all(self.error == 0.):
+            self.mag += np.random.normal(0.0, self.error)
