e6531c7dbce67600a66a8dadcaafad8f8787e520
adrn <adrian.prw@gmail.com>
2012-10-21 23:00:31 -0400

    remove c file (generated from cython) and modify setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,21 @@
 from distutils.core import setup, Extension
+from Cython.Distutils import build_ext
+
+# Get numpy path
+import os, numpy
+numpy_base_path = os.path.split(numpy.__file__)[0]
+numpy_incl_path = os.path.join(numpy_base_path, "core", "include")
 
 error_func = Extension('ptf/analyze/error_functions',
-                                sources=['ptf/analyze/error_functions.c'],
-                                include_dirs=['/scr4/dlevitan/sw/epd-7.1-1-rh5-x86_64/lib/python2.7/site-packages/numpy/core/include'])
+                        sources=['ptf/analyze/error_functions.c'],
+                        include_dirs=[numpy_incl_path])
+
+aov = Extension('ptf.analyze.aov',
+                sources=['ptf/analyze/aov.pyx'],
+                include_dirs=[numpy_incl_path])
 
 setup(name='PTF',
-      version='1.0',
+      version='2.0',
       description="adrn's PTF project",
-      ext_modules=[error_func])
+      cmdclass={'build_ext': build_ext},
+      ext_modules=[error_func, aov])
