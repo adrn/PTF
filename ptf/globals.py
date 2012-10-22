4dc81e426b34ac93501f458aaf88f68853d78ec5
adrn <adrian.prw@gmail.com>
2012-10-21 22:46:01 -0400

    move functions from globals to util

--- ptf/globals.py
+++ ptf/globals.py
@@ -16,8 +16,6 @@ ogle_camera_size = (1.225,0.9) #degrees
 index_to_label = {"j" : "J", "k" : "K", "sigma_mu" : r"$\sigma/\mu$", "eta" : r"$\eta$", "delta_chi_squared" : r"$\Delta \chi^2$", "con" : "Con", "corr" : "Corr"}
 
 config = dict()
-
-#config_filename = os.path.
 _base_path = os.path.split(__file__)[0]
 
 with open(os.path.join(_base_path, "config"), "r") as f:
@@ -27,18 +25,5 @@ with open(os.path.join(_base_path, "config"), "r") as f:
             config[key] = int(val)
         except ValueError:
             config[key] = val
-
-# Convert my names for variability indices to the PDB names
-pdb_index_name = dict(eta="vonNeumannRatio", j="stetsonJ", k="stetsonK", delta_chi_squared="chiSQ", sigma_mu=["magRMS","referenceMag"])
-
-def source_index_name_to_pdb_index(source, index_name):
-    """ Given a source (a row from chip.sources) and an index name (e.g. eta),
-        return the value of the statistic. This is particularly needed for a
-        computed index like sigma_mu.
-    """
-    if index_name == "sigma_mu":
-        return source[pdb_index_name[index_name][0]] / source[pdb_index_name[index_name][1]]            
-    else:
-        return source[pdb_index_name[index_name]]
 
 all_fields = np.load(os.path.join(os.path.split(_base_path)[0], "data", "all_fields.npy"))
--- ptf/util.py
+++ ptf/util.py
@@ -4,39 +4,19 @@
 # Standard library
 import os, sys
 
-# Third-party
-import numpy as np
-import matplotlib.pyplot as plt
+# Convert my names for variability indices to the PDB names
+pdb_index_name = dict(eta="vonNeumannRatio", \
+                      j="stetsonJ", \
+                      k="stetsonK", \
+                      delta_chi_squared="chiSQ", \
+                      sigma_mu=["magRMS","referenceMag"])
 
-# Project
-from ptf.db.DatabaseConnection import *
-import ptf.simulation.util as simu
-from ptf import PTFLightCurve
-
-parameter_to_label = {"j" : "J", "k" : "K", "sigma_mu" : r"$\sigma/\mu$", "eta" : r"$\eta$", "delta_chi_squared" : r"$\Delta \chi^2$"}
-
-def txt_file_light_curve_to_recarray(filename):
-    """ Load Marcel / Nick's text file light curves into a Numpy recarray.
-    
-        All columns in this file are:
-            MJD
-            R1	<-- ignore (I think; doesn't matter much
-            R2	which of these two you use for this, I say)
-            R2 err
-            RA
-            DEC
-            X
-            Y
-            flag
-            file
-            field
-            chip
-            
-            MJD's are relative to 2009-1-1 (54832)
+def source_index_name_to_pdb_index(source, index_name):
+    """ Given a source (a row from chip.sources) and an index name (e.g. eta),
+        return the value of the statistic. This is particularly needed for a
+        computed index like sigma_mu.
     """
-    
-    names = ["mjd", "mag", "mag_err", "ra", "dec", "flag", "fieldid", "ccd"]
-    usecols = [0, 2, 3, 4, 5, 8, 10, 11]
-    dtypes = [float, float, float, float, float, float, int, int]
-    
-    return np.genfromtxt(filename, usecols=usecols, dtype=zip(names, dtypes)).view(np.recarray)
+    if index_name == "sigma_mu":
+        return source[pdb_index_name[index_name][0]] / source[pdb_index_name[index_name][1]]            
+    else:
+        return source[pdb_index_name[index_name]]
