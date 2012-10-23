cb0473097a2c99ccdc90e4aa19f29c9edb2ddf12
adrn <adrian.prw@gmail.com>
2012-10-23 10:57:28 -0700

    fix typos

--- ptf/variability_indices.py
+++ ptf/variability_indices.py
@@ -12,7 +12,9 @@ import numpy as np
 
 from .analyze import compute_variability_indices
 from .lightcurve import SimulatedLightCurve
-from .util import get_logger
+from .simulation import simulate_light_curves_compute_indices
+from .util import get_logger, source_index_name_to_pdb_index, richards_qso
+from .globals import min_number_of_good_observations
 logger = get_logger(__name__)
 
 def prune_index_distribution(index_array, index):
@@ -82,10 +84,10 @@ def var_indices_for_simulated_light_curves(field, number_of_light_curves, number
     db_statistic_values = dict()
     for index in indices:
         db_statistic_values[index] = np.array([])
-        
+    
+    indent_level += "\t"
     for ccd in field.ccds.values():
         logger.info(indent_level+"Starting with CCD {}".format(ccd.id))
-        indent_level += "\t"
         chip = ccd.read()
         
         logger.debug(indent_level + "Getting variability statistics from photometric database")
@@ -103,7 +105,7 @@ def var_indices_for_simulated_light_curves(field, number_of_light_curves, number
         for index in indices:
             # This is where I need to define the selection distributions for each index.
             db_statistic_values[index] = np.append(db_statistic_values[index], 
-                                                   vi.prune_index_distribution(db_statistics_array[index], index))
+                                                   prune_index_distribution(db_statistics_array[index], index))
         
         # Up to here I have just selected the original variability statistic distributions from the
         #   photometric database and appended them to db_statistic_values. Next I will run simulations
@@ -117,17 +119,19 @@ def var_indices_for_simulated_light_curves(field, number_of_light_curves, number
         # Keep count of how many light curves we've used, break after we reach the specified number
         light_curve_count = 0
         for source_id in source_ids:
+            #for light_curve in ccd.light_curves(source_ids, clean=True): # clean applies a quality cut to the data
             light_curve = ccd.light_curve(source_id, barebones=True, clean=True) # clean applies a quality cut to the data
+            #print "loaded"
             
             if light_curve == None or len(light_curve) < min_number_of_good_observations: 
                 # If the light curve is not found, or has too few observations, skip this source_id
                 continue
                 
-            simulated_indices_for_this_light_curve = sim.simulate_light_curves_compute_indices(light_curve, \
+            simulated_indices_for_this_light_curve = simulate_light_curves_compute_indices(light_curve, \
                                                                                 num=number_of_simulations_per_light_curve, \
                                                                                 indices=indices)
             try:
-                simulated_statistics = np.hstack((simulated_light_curve_statistics, simulated_indices_for_this_light_curve))
+                simulated_statistics = np.hstack((simulated_statistics, simulated_indices_for_this_light_curve))
             except NameError:
                 simulated_statistics = simulated_indices_for_this_light_curve
                 
@@ -160,6 +164,7 @@ def compute_selection_criteria(var_indices, indices, fpr=0.01):
         
     """
     
+    indent_level = "\t"
     try:
         if var_indices == None or len(var_indices["simulated"]) == 0:
             raise ValueError
@@ -167,7 +172,6 @@ def compute_selection_criteria(var_indices, indices, fpr=0.01):
         logger.info(indent_level + "No statistics computed for this field! Something went wrong...")
         return None
     
-    indent_level = "\t"
     logger.debug(indent_level + "Starting false positive rate calculation to determine selection criteria")
     
     # Now determine the N in N-sigma by computing the false positive rate and getting it to be ~0.01 (1%) for each index
@@ -176,12 +180,12 @@ def compute_selection_criteria(var_indices, indices, fpr=0.01):
         logger.debug(indent_level + "Index: {}".format(index))
         
         # Get the mean and standard deviation of the 'vanilla' distributions
-        db_mean, db_sigma = np.mean(np.log10(db_statistic_values[index])), np.std(np.log10(db_statistic_values[index]))
+        db_mean, db_sigma = np.mean(np.log10(var_indices["db"][index])), np.std(np.log10(var_indices["db"][index]))
         logger.debug(indent_level + "\t mu={}, sigma={}".format(db_mean, db_sigma))
         
         # Get the simulated statistics for this index 
         #TODO: Check the validity of using prune_ here -- might be some issue with J
-        these_statistics = np.log10(vi.prune_index_distribution(simulated_statistics[index]))
+        these_statistics = np.log10(prune_index_distribution(var_indices["simulated"][index],index))
         
         # Start by selecting with Nsigma = 0
         Nsigma = 0.
