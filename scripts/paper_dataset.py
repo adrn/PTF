65dd9799235aa5b0c0ef93560b5d09489d9e7731
adrn <adrian.prw@gmail.com>
2013-10-07 16:27:39 -0400

    find 100000 random field, ccd pairs

--- scripts/paper_dataset.py
+++ scripts/paper_dataset.py
@@ -1,1 +1,55 @@
+# coding: utf-8
+from __future__ import division
 
+""" Select 100000 random light curves from PTF to publish. """
+
+__author__ = "adrn <adrn@astro.columbia.edu>"
+
+# Standard library
+import os, sys
+import logging
+import random
+
+# Third-party
+import numpy as np
+
+# Project
+import ptf.db.photometric_database as pdb
+from ptf.globals import all_fields
+
+np.random.seed(42)
+Nfields = len(all_fields)
+Nlightcurves = 100000
+Nselected = 0
+
+ccd_ids = range(13)
+ccd_ids.pop(3)
+
+field_ccd = dict()
+while Nselected < Nlightcurves:
+    field_row = all_fields[np.random.randint(Nfields)]
+    field_id = field_row['id']
+    ccd_id = random.choice(ccd_ids)
+
+    print(field_id, ccd_id)
+
+    try:
+        ccd = pdb.CCD(ccd_id, field_id, 'R')
+    except ValueError:
+        continue
+
+    try:
+        field_ccd[field_id]
+    except KeyError:
+        field_ccd[field_id] = dict()
+
+    field = field_ccd[field_id]
+
+    try:
+        field[ccd_id]
+    except KeyError:
+        field[ccd_id] = 0
+
+    field_ccd[field_id][ccd_id] += 1
+    Nlightcurves += 1
+    sys.exit(0)    
