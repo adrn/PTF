ab12e91f355fa674ba212e7aae6ecee79aaba04d
adrn <adrian.prw@gmail.com>
2013-10-16 14:16:53 -0700

    make into function

--- scripts/paper_dataset.py
+++ scripts/paper_dataset.py
@@ -19,36 +19,40 @@ import ptf.db.photometric_database as pdb
 from ptf.globals import all_fields, data_path
 
 np.random.seed(42)
-Nfields = len(all_fields)
-Nlightcurves = 100000
-Nselected = 0
 
-ccd_ids = range(13)
-ccd_ids.pop(3)
+def make_lookup_dict(Nlightcurves=100000):
+    Nfields = len(all_fields)
+    Nlightcurves = 100000
+    Nselected = 0
 
-field_ccd = dict()
-while Nselected < Nlightcurves:
-    field_row = all_fields[np.random.randint(Nfields)]
-    field_id = field_row['id']
+    ccd_ids = range(13)
+    ccd_ids.pop(3)
     
-    field = pdb.Field(field_id, 'R')
-    if len(field.ccds) == 0:
-        continue
+    field_ccd = dict()
+    while Nselected < Nlightcurves:
+        field_row = all_fields[np.random.randint(Nfields)]
+        field_id = field_row['id']
+    
+        field = pdb.Field(field_id, 'R')
+        if len(field.ccds) == 0:
+            continue
 
-    ccd = random.choice(field.ccds.values())
+        ccd = random.choice(field.ccds.values())
 
-    try:
-        field_ccd[field.id]
-    except KeyError:
-        field_ccd[field.id] = dict()
+        try:
+            field_ccd[field.id]
+        except KeyError:
+            field_ccd[field.id] = dict()
 
-    try:
-        field_ccd[field_id][ccd.id]
-    except KeyError:
-        field_ccd[field_id][ccd.id] = 0
+        try:
+            field_ccd[field_id][ccd.id]
+        except KeyError:
+            field_ccd[field_id][ccd.id] = 0
 
-    field_ccd[field.id][ccd.id] += 1
-    Nselected += 1
+            field_ccd[field.id][ccd.id] += 1
+            Nselected += 1
 
-with open(os.path.join(data_path, "publish_field_ccds.pickle"), 'w') as f:
-    pickle.dump(field_ccd, f)
+    with open(os.path.join(data_path, "publish_field_ccds.pickle"), 'w') as f:
+        pickle.dump(field_ccd, f)
+   
+def 
