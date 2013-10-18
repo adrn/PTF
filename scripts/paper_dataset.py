f68e2c298bc8ea620ce3f7be89b55e947ae1794f
adrn <adrian.prw@gmail.com>
2013-10-18 12:30:41 -0400

    wrote function to store light curves

--- scripts/paper_dataset.py
+++ scripts/paper_dataset.py
@@ -13,12 +13,14 @@ import cPickle as pickle
 
 # Third-party
 import numpy as np
+from astropy.io.misc import fnunpickle
 
 # Project
 import ptf.db.photometric_database as pdb
 from ptf.globals import all_fields, data_path
 
 np.random.seed(42)
+dict_file = os.path.join(data_path, "publish_field_ccds.pickle")
 
 def make_lookup_dict(Nlightcurves=100000):
     Nfields = len(all_fields)
@@ -52,7 +54,64 @@ def make_lookup_dict(Nlightcurves=100000):
             field_ccd[field.id][ccd.id] += 1
             Nselected += 1
 
-    with open(os.path.join(data_path, "publish_field_ccds.pickle"), 'w') as f:
+    with open(dict_file, 'w') as f:
         pickle.dump(field_ccd, f)
    
-def 
+def random_light_curves(ccd, N):
+    chip = ccd.read()
+    sources = chip.sources.readWhere("ngoodobs > 10")
+    np.random.shuffle(sources)
+
+    lcs = []
+    for source in sources:
+        lc = ccd.light_curve(source["matchedSourceID"], clean=True, barebones=True)
+        if len(lc) > 10:
+            lcs.append(lc)
+
+        if len(lcs) >= N:
+            return lcs
+
+def store_light_curves():
+    field_ccd = fnunpickle(dict_file)
+
+    all_lightcurves = []
+    for ii,field_id in enumerate(sorted(field_ccd.keys())):
+        this_file = os.path.join(data_path, "publish_light_curves", "{0}.fits".format(ii % 100))
+        if os.path.exists(this_file):
+            continue
+
+        field = pdb.Field(field_id, 'R')
+
+        for ccd_id in sorted(field_ccd[field_id].keys()):
+            num = field_ccd[field_id][ccd_id]
+            ccd = field.ccds[ccd_id]
+
+            lcs = random_light_curves(ccd, num)
+            all_lightcurves += lcs
+
+        if ii > 0 and (ii % 100) == 0:
+            #write to file
+
+            data = None
+            for lc in all_lightcurves:
+                n = len(lc.mjd)
+                f = [lc.field_id]*n
+                c = [lc.ccd_id]*n
+                s = [lc.source_id]*n
+                
+                if data is None:
+                    data = np.array([f, c, s, lc.mjd, lc.mag, lc.error])
+                else:
+                    data = np.hstack((data, np.array([f, c, s, lc.mjd, lc.mag, lc.error]))
+
+            print(data.shape)
+            sys.exit(0)
+
+            all_lightcurves = []
+
+
+if __name__ == "__main__":
+    if not os.path.exists(dict_file):
+        make_lookup_dict(Nlightcurves=100000)
+
+    store_light_curves()
