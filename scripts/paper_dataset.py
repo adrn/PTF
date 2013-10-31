b31c269d5088596af34478c2336a1b515324dd6d
adrn <adrian.prw@gmail.com>
2013-10-31 15:28:40 -0400

    bork

--- scripts/paper_dataset.py
+++ scripts/paper_dataset.py
@@ -35,47 +35,43 @@ def get_lcs(Nlightcurves=1000, seed=42):
     while Nselected < Nlightcurves:
         field_row = all_fields[np.random.randint(Nfields)]
         field_id = field_row['id']
-        
+
         field = pdb.Field(field_id, 'R')
-        
+
         if len(field.ccds) == 0:
             continue
-        
+
         if not cache[field_id].has_key(ccd.id):
             ccd = random.choice(field.ccds.values())
             chip = ccd.read()
             sources = chip.sources.readWhere("ngoodobs > 10")
             cache[field_id][ccd.id] = sources
-        
+
         np.random.shuffle(cache[field_id][ccd.id])
-        
+
         for source in cache[field_id][ccd.id]:
             lc = ccd.light_curve(source["matchedSourceID"], clean=True, barebones=True)
             if len(lc) > 10:
                 lcs.append(lc)
-    
+
     return lcs
 
 def store_light_curves(lcs):
+    data = None
     for lc in lcs:
-                n = len(lc.mjd)
-                f = [lc.field_id]*n
-                c = [lc.ccd_id]*n
-                s = [lc.source_id]*n
-                
-                if data is None:
-                    data = np.array([f, c, s, lc.mjd, lc.mag, lc.error])
-                else:
-                    data = np.hstack((data, np.array([f, c, s, lc.mjd, lc.mag, lc.error])))
+        n = len(lc.mjd)
+        f = [lc.field_id]*n
+        c = [lc.ccd_id]*n
+        s = [lc.source_id]*n
 
-            print(data.shape)
-            sys.exit(0)
+        if data is None:
+            data = np.array([f, c, s, lc.mjd, lc.mag, lc.error])
+        else:
+            data = np.hstack((data, np.array([f, c, s, lc.mjd, lc.mag, lc.error])))
 
-            all_lightcurves = []
-
+    print(data.shape)
+    sys.exit(0)
 
 if __name__ == "__main__":
-    if not os.path.exists(dict_file):
-        make_lookup_dict(Nlightcurves=10000)
-
-    store_light_curves()
+    lcs = get_lcs(100, seed=42)
+    store_light_curves(lcs)
